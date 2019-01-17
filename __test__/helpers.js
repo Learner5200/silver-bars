7b9a877cf67fed1a77a92f5cde0cadbb3ba8eb35
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-17 16:00:31 +0000

    Refactor helpers to export individual build functions rather than class

--- __test__/helpers.js
+++ __test__/helpers.js
@@ -1,15 +1,13 @@
-export class Builder {
-  orderParams({
-    quantity = 1,
-    price = 100,
-    userID = 'user1',
-    type = undefined,
-  }) {
-    return {
-      quantity,
-      price,
-      userID,
-      type,
-    };
-  }
+export function buildOrderParams({
+  quantity = 1,
+  price = 100,
+  userID = 'user1',
+  type = undefined,
+}) {
+  return {
+    quantity,
+    price,
+    userID,
+    type,
+  };
 }
