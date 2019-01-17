ddd130590b4f0a29c20aa814d8a9c33b37846ea4
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-17 14:19:23 +0000

    Test that OrderInterface.buy() returns order ID

--- __test__/orderInterface.test.js
+++ __test__/orderInterface.test.js
@@ -3,25 +3,32 @@ import OrderInterface from '../src/orderInterface';
 describe('OrderInterface', () => {
   let orderInterface;
   let mockOrderBoard;
+  let buyParams;
   beforeEach(() => {
     mockOrderBoard = {
-      register() {},
+      register() {
+        return 1;
+      },
     };
     orderInterface = new OrderInterface({
       orderBoard: mockOrderBoard,
     });
+    buyParams = {
+      quantity: 1,
+      price: 100,
+      userID: 'user1',
+    };
   });
   describe('when buying silver bars', () => {
     it('registers a new order on order board of type "BUY"', () => {
-      const buyParams = {
-        quantity: 3.5,
-        price: 303,
-        userID: 'user1',
-      };
       const registerParams = Object.assign(buyParams, { type: 'BUY' });
       const registerSpy = jest.spyOn(mockOrderBoard, 'register');
       orderInterface.buy(buyParams);
       expect(registerSpy).toHaveBeenCalledWith(registerParams);
+    });
+    it('returns the return value of OrderBoard.register() as order ID', () => {
+      const id = orderInterface.buy(buyParams);
+      expect(id).toBe(1);
     });
   });
 });
