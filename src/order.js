4c3ba9877d76c860856c78170c370ce097f1d6fa
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-18 09:09:00 +0000

    Order initializes with correct properties

--- src/order.js
+++ src/order.js
@@ -1,3 +1,15 @@
 export default class Order {
-
+  constructor({
+    quantity,
+    price,
+    userID,
+    type,
+    ID,
+  }) {
+    this.quantity = quantity;
+    this.price = price;
+    this.userID = userID;
+    this.type = type;
+    this.ID = ID;
+  }
 }
