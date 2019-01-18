78f30e2b23b5aad857dd50861377883e6a820a34
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-18 17:55:31 +0000

    Write failing feature test for selling bars

--- __test__/features/buying.test.js
+++ __test__/features/buying.test.js
@@ -5,7 +5,7 @@ describe('Buying', () => {
   beforeEach(() => {
     orderInterface = new OrderInterface();
   });
-  test('user can register an order to buy silver bars', () => {
+  test('user can register an order to buy silver bars and see this on the display', () => {
     orderInterface.buy({
       quantity: 3.5,
       price: 303,
@@ -14,5 +14,15 @@ describe('Buying', () => {
     const spy = jest.spyOn(console, 'log');
     orderInterface.display();
     expect(spy).toHaveBeenCalledWith('LIVE ORDER BOARD\n\nBUY:\n\n3.5kg for £303\n\n');
+  });
+  test('user can register an order to sell silver bars and see this on the display', () => {
+    orderInterface.sell({
+      quantity: 3.5,
+      price: 303,
+      userID: 'user1',
+    });
+    const spy = jest.spyOn(console, 'log');
+    orderInterface.display();
+    expect(spy).toHaveBeenCalledWith('LIVE ORDER BOARD\n\nSELL:\n\n3.5kg for £303\n\n');
   });
 });
