7728676d7561aab5a63f679ae954a1fb11e46195
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-18 18:07:39 +0000

    Added feature tests for sorting of orders in display

--- __test__/features/buying.test.js
+++ __test__/features/buying.test.js
@@ -6,17 +6,19 @@ describe('Buying', () => {
   beforeEach(() => {
     orderInterface = new OrderInterface();
   });
-  test('user can register an order to buy silver bars and see this on the display', () => {
+  test('user can register orders to buy silver bars and see them in descending price order on the display', () => {
     orderInterface.buy(buildOrderParams());
+    orderInterface.buy(buildOrderParams({ quantity: 2, price: 200 }));
     const spy = jest.spyOn(console, 'log');
     orderInterface.display();
-    expect(spy).toHaveBeenCalledWith('LIVE ORDER BOARD\n\nBUY:\n\n1kg for £100\n\n');
+    expect(spy).toHaveBeenCalledWith('LIVE ORDER BOARD\n\nBUY:\n\n2kg for £200\n1kg for £100\n\n');
   });
-  test('user can register an order to sell silver bars and see this on the display', () => {
+  test('user can register orders to sell silver bars and see them in ascending price order on the display', () => {
     orderInterface.sell(buildOrderParams());
+    orderInterface.sell(buildOrderParams({ quantity: 2, price: 200 }));
     const spy = jest.spyOn(console, 'log');
     orderInterface.display();
-    expect(spy).toHaveBeenCalledWith('LIVE ORDER BOARD\n\nSELL:\n\n1kg for £100\n\n');
+    expect(spy).toHaveBeenCalledWith('LIVE ORDER BOARD\n\nSELL:\n\n1kg for £100\n2kg for £200\n\n');
   });
   test('user can both buy and sell silver bars and see them in separate places on the display', () => {
     orderInterface.buy(buildOrderParams());
