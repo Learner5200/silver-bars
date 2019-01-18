7ab12514e83907dca52b04a2028b8044427c4725
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-18 17:06:23 +0000

    Extract OrderBoardSummariser to generate aggregate orders
    Refactored this logic out of the OrderBoardView and into a separate 
    class

---
+++ __test__/orderBoardSummariser.test.js
@@ -0,0 +1,94 @@
+import OrderBoardSummariser from '../src/orderBoardSummariser';
+import { buildOrderParams } from './helpers';
+
+describe('OrderBoardSummariser', () => {
+  let summariser;
+  let fakeOrderBoard;
+  let summary;
+  class MockAggregateOrder {
+    constructor({
+      quantity,
+      price,
+    }) {
+      this.quantity = quantity;
+      this.price = price;
+    }
+  }
+
+  beforeEach(() => {
+    fakeOrderBoard = {
+      getOrders: () => [
+        buildOrderParams({
+          quantity: 1,
+          price: 100,
+        }),
+        buildOrderParams({
+          quantity: 1,
+          price: 100,
+        }),
+        buildOrderParams({
+          quantity: 1,
+          price: 200,
+        }),
+        buildOrderParams({
+          quantity: 10,
+          price: 1000,
+          type: 'SELL',
+        }),
+        buildOrderParams({
+          quantity: 10,
+          price: 1000,
+          type: 'SELL',
+        }),
+        buildOrderParams({
+          quantity: 20,
+          price: 2000,
+          type: 'SELL',
+        }),
+      ],
+    };
+    summariser = new OrderBoardSummariser({
+      AggregateOrderClass: MockAggregateOrder,
+    });
+    summary = summariser.summarise({
+      orderBoard: fakeOrderBoard,
+      type: 'BUY',
+    });
+  });
+
+  describe('.summarise()', () => {
+    it('returns array of aggregate orders', () => {
+      expect(summary[0]).toBeInstanceOf(MockAggregateOrder);
+    });
+    it('aggregates orders at same price', () => {
+      expect(summary.length).toBe(2);
+      expect(summary[1].quantity).toBe(2);
+    });
+
+    describe('when type === BUY', () => {
+      it('contains only BUY orders', () => {
+        const sellOrders = summary.filter(order => order.quantity >= 10);
+        expect(sellOrders).toEqual([]);
+      });
+      it('sorts descending by price', () => {
+        expect(summary[0].price).toBeGreaterThan(summary[1].price);
+      });
+    });
+
+    describe('when type === SELL', () => {
+      beforeEach(() => {
+        summary = summariser.summarise({
+          orderBoard: fakeOrderBoard,
+          type: 'SELL',
+        });
+      });
+      it('contains only SELL orders', () => {
+        const buyOrders = summary.filter(order => order.quantity < 10);
+        expect(buyOrders).toEqual([]);
+      });
+      it('sorts ascending by price', () => {
+        expect(summary[0].price).toBeLessThan(summary[1].price);
+      });
+    });
+  });
+});
---
+++ src/orderBoardSummariser.js
@@ -0,0 +1,45 @@
+import AggregateOrder from './aggregateOrder';
+
+export default class OrderBoardSummariser {
+  constructor({
+    AggregateOrderClass = AggregateOrder,
+  }) {
+    this.AggregateOrderClass = AggregateOrderClass;
+  }
+
+  summarise({
+    orderBoard,
+    type,
+  }) {
+    const orderList = orderBoard.getOrders();
+    const filteredOrders = this.filter({ orderList, type });
+    const aggregateOrders = this.aggregateOrderList(filteredOrders);
+    if (type === 'BUY') aggregateOrders.reverse();
+    return aggregateOrders;
+  }
+
+  filter({
+    orderList,
+    type,
+  }) {
+    return orderList.filter(order => order.type === type);
+  }
+
+  aggregateOrderList(orders) {
+    const groupedOrders = this.groupByPrice(orders);
+    const prices = Object.keys(groupedOrders);
+    return prices.map(price => new this.AggregateOrderClass({
+      price: Number(price),
+      quantity: groupedOrders[price],
+    }));
+  }
+
+  groupByPrice(orders) {
+    const reducer = (summary, order) => {
+      const newSummary = summary;
+      newSummary[order.price] = newSummary[order.price] + order.quantity || order.quantity;
+      return newSummary;
+    };
+    return orders.reduce(reducer, {});
+  }
+}
