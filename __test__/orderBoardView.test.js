7a444e2260eac4125215381102cffb09ed9a9f27
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-18 10:43:24 +0000

    Test that OrderBoardView renders BUY orders in descending price order

---
+++ __test__/orderBoardView.test.js
@@ -0,0 +1,27 @@
+import OrderBoardView from '../src/orderBoardView';
+import { buildOrderParams } from './helpers';
+
+describe('OrderBoardView', () => {
+  let fakeOrderBoard;
+  let orderBoardView;
+  beforeEach(() => {
+    fakeOrderBoard = {
+      getOrders: () => [
+        buildOrderParams({
+          price: 200,
+        }),
+        buildOrderParams({
+          price: 300,
+        }),
+      ],
+    };
+    orderBoardView = new OrderBoardView();
+  });
+
+  describe('.render()', () => {
+    it('renders BUY orders in descending price order', () => {
+      const output = orderBoardView.render(fakeOrderBoard);
+      expect(output).toBe('LIVE ORDER BOARD\n\nBUY:\n\n1kg for £200\n1kg for £300\n\nSELL:');
+    });
+  });
+});
